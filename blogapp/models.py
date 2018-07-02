c9cc648fcf47b528850c9338e37d39ae39735a51
smallren101 <small_pupil@126.com>
2018-07-02 11:07:07 +0800

    models已创建

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# 创建种类表
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=24)
+
+# 创建文章表
+class Article(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    content = models.CharField(max_length=1024)
+    author = models.CharField(max_length=24)
+    write_time = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # 一个种类可对应多篇文章
